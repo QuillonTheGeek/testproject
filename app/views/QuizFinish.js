b6ff4f9213a86bc84b771072f6e0ae7fc8af9354
Quillon <quillonthegeek@gmail.com>
2023-08-19 18:40:57 +0100

    QuizFinish view

---
+++ app/views/QuizFinish.js
@@ -0,0 +1,36 @@
+import { TouchableHighlight, View } from "react-native";
+
+const QuizFinish = ({ route, navigation }) => {
+  const { score, missed, questions } = route.params;
+
+  const exitQuiz = () => {
+    navigation.navigate("Home");
+  };
+
+  return (
+    <View style={styles.coontainer}>
+      <Text>Your quiz score was {score}</Text>
+      <Text>
+        Your missed on {missed} out of {questions} questions
+      </Text>
+
+      <TouchableHighlight onPress={exitQuiz} style={styles.button}>
+        <Text>Finish</Text>
+      </TouchableHighlight>
+    </View>
+  );
+};
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 30,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  button: {
+    alignItems: "center",
+    justifyContent: "center",
+    height: "10%",
+  },
+});
+export default QuizFinish;
